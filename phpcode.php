badee9cfee2a05c2f684df61be4501092384cf39
Dm-Smirny <84511770+Dm-Smirny@users.noreply.github.com>
2021-05-24 15:40:58 +0300

    SPA-4

--- phpcode.php
+++ phpcode.php
@@ -2,7 +2,7 @@
 
 $array = array("1" => 'Hello',  
               "2" => 'Username', 
-              "3" => 'Have a good day'
+              "3" => 'Have a good day, cool'
               );
               
 foreach( $array as $key => $value ){
