c3e09b77b286387bca051f6beb6b66e372ebd5bc
Dm-Smirny <84511770+Dm-Smirny@users.noreply.github.com>
2021-05-24 11:50:54 +0300

    SPA-4

---
+++ phpcode.php
@@ -0,0 +1,10 @@
+<?php
+
+$array = array("1" => 'Hello',  
+              "2" => 'Username', 
+              "3" => 'Have a good day'
+              );
+              
+foreach( $array as $key => $value ){
+    echo "$value ";
+}
